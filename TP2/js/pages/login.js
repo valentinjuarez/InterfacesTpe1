0fa8d9d4c52c6b88a098676b12a5d73cc60e00ec
Valentin Juarez <valenjua06@gmail.com>
2025-10-07 16:03:56 -0300

    muevo el index.html de la entrega 1

--- TP2/js/pages/login.js
+++
@@ -1,6 +0,0 @@
-import { setupPasswordVisibility } from '../features/password-visibility.js';
-import { setupEmailValidation } from '../features/email-validation.js';
-
-// Inicializar comportamientos de la página de login
-setupPasswordVisibility();
-setupEmailValidation();
