5d7c0810ea2d68fc8fefeba301b7cc25215da506
lautaroAlegre <lalegrezelione@alumnos.exa.unicen.edu.ar>
2025-10-15 17:48:16 -0300

    .

---
+++ TP3/js/canva.js
@@ -0,0 +1,6 @@
+function initCanvas() {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  // Aquí puedes agregar el código para inicializar el lienzo
+}
